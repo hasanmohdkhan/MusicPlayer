7531d15a1e7732119f5e5aebbba3eea4f8388927
hasanmohdkhan <hasanzian@gmail.com>
2017-12-26 22:46:37 +0530

    UI polish 3 screen

---
+++ app/src/main/java/com/example/hasanzian/hackerearth/HomeActivity.java
@@ -0,0 +1,18 @@
+package com.example.hasanzian.hackerearth;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by hasanZian on 26-12-2017.
+ */
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_screen_activity);
+    }
+
+}
